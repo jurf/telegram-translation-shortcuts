bdbbc93a220e3523b93eee955c0ae7a8de56a3d4
Juraj Fiala <jurf@riseup.net>
2019-05-27 23:39:07 +0200

    Refactor, reformat
    This will get larger, account for that. VS Code format defaults (sans max width).

--- telegram-translation-shortcuts.user.js
+++ telegram-translation-shortcuts.user.js
@@ -10,15 +10,18 @@
 // ==/UserScript==
 
 // Quick apply
-document.addEventListener("keyup",
-    function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            document.getElementsByClassName("btn btn-sm btn-default key-status-apply-btn")[0].click();
-            var e = new KeyboardEvent("keydown", {
-                which: Keys.DOWN
-            });
-            LangKeys.onKeyDown(e);
-        }
-    }
-);
+document.addEventListener('keyup', handleEvent);
+
+function handleEvent(e) {
+  if (e.key === 'Enter') {
+    event.preventDefault();
+    applyString();
+  }
+}
+
+function applyString() {
+  document
+      .getElementsByClassName('btn btn-sm btn-default key-status-apply-btn')[0]
+      .click();
+  LangKeys.onKeyDown(new KeyboardEvent('keydown', {which: Keys.DOWN}));
+}

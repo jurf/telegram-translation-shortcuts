4ba5b461d13eb57ca9003e6ee1c629f6d5e5fd77
Juraj Fiala <jurf@riseup.net>
2019-05-27 22:34:23 +0200

    Add initial version of the script

---
+++ telegram-translation-shortcuts.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Telegram Translation Platform Shortcuts
+// @namespace    https://github.com/jurf/telegram-translation-shortcuts
+// @description  Adds useful keyboard shortcuts to the Telegram Translation Platform
+// @include      https://translations.telegram.org/*
+// @version      0.1.0
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/jurf/telegram-translation-shortcuts/master/telegram-translation-shortcuts.user.js
+// @updateURL    https://raw.githubusercontent.com/jurf/telegram-translation-shortcuts/master/telegram-translation-shortcuts.user.js
+// ==/UserScript==
+
+// Quick apply
+document.addEventListener("keyup",
+    function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementsByClassName("btn btn-sm btn-default key-status-apply-btn")[0].click();
+            var e = new KeyboardEvent("keydown", {
+                which: Keys.DOWN
+            });
+            LangKeys.onKeyDown(e);
+        }
+    }
+);

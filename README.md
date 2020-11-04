5b89580fc0d6052f424a1fc8dff5998a9b5de647
rondevous <67483423+rondevous@users.noreply.github.com>
2020-11-04 07:40:22 +0530

    Use Ctrl+A to select all phrases on import page
    Also:
    - created methods to hide suggestions for a possible new feature

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Telegram Translation Platform Shortcuts
+⚠️ **Warning! You are on the unstable development branch. [Please go to the stable branch](https://github.com/jurf/telegram-translation-shortcuts/tree/master)**
 
-Adds useful keyboard shortcuts to the Telegram Translation Platform.
+# Telegram Translation Platform Shortcuts (dev)
+
+Adds useful keyboard shortcuts and enhances your experience on Telegram's Translation Panel.
 
 If you run out of clicking fingers, consider giving this userscript a try.
 The shortcuts are aimed at anyone who uses the [Translations website](https://translations.telegram.org), as translator, contributor, or as a reviewer when applying untranslated strings.
@@ -15,18 +17,19 @@ It’s currently an early work-in-progress, but you might still find it handy. C
 | `1` – `5`                             | Apply the n-th suggestion & move to next
 | `Tab` / `Shift+Tab` (or `h` / `l`)    | Cycle linked phrases/strings
 | `PageUp` / `PageDown` (or `k` / `j`)  | Move up/down
-| `Ctrl+A` (or `i`)                     | Add a new translation
+| `Ctrl+A` (or `i`)                     | Clicks on "Add Translation"
 | `Ctrl+E` (or `c`)                     | Edit the first suggestion
 | `/`                                   | Search
-| `Ctrl+Enter`                          | Submit translation (or comment)
-| `Esc`                                 | Cancel translation (or comment)
+| `Ctrl+Enter`                          | Send or submit a translation/comment
+| `Esc`                                 | Cancel
+| `Ctrl+A`                              | Select/unselect all phrases on the import page
 
 …and more to come.
 
 ## Installation
 
 1. Install [Violentmonkey](https://violentmonkey.github.io/get-it/), or similar browser extension
-2. [Install](https://github.com/jurf/telegram-translation-shortcuts/raw/master/telegram-translation-shortcuts.user.js) the userscript
+2. [Install](https://github.com/jurf/telegram-translation-shortcuts/raw/dev/telegram-translation-shortcuts.user.js) the userscript
 3. Enjoy! :slightly_smiling_face:
 
 ## Support
--- telegram-translation-shortcuts.user.js
+++ telegram-translation-shortcuts.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         Telegram Translation Platform Shortcuts
-// @namespace    https://github.com/jurf/telegram-translation-shortcuts
+// @name         Telegram Translation Shortcuts (dev)
+// @namespace    https://github.com/jurf/telegram-translation-shortcuts/dev
 // @description  Adds useful keyboard shortcuts to the Telegram Translation Platform
 // @author       Juraj Fiala
 // @include      https://translations.telegram.org/*
 // @version      0.4.1
 // @grant        none
 // @run-at       document-start
-// @downloadURL  https://github.com/jurf/telegram-translation-shortcuts/raw/master/telegram-translation-shortcuts.user.js
-// @updateURL    https://github.com/jurf/telegram-translation-shortcuts/raw/master/telegram-translation-shortcuts.user.js
+// @downloadURL  https://github.com/jurf/telegram-translation-shortcuts/raw/dev/telegram-translation-shortcuts.user.js
+// @updateURL    https://github.com/jurf/telegram-translation-shortcuts/raw/dev/telegram-translation-shortcuts.user.js
 // ==/UserScript==
 
 /* global LangKeys, KeyboardEvent, Keys */
@@ -170,10 +170,54 @@ function submit () {
   if (inCommentField()) {
     const formbtns = document.activeElement.nextElementSibling.children.item(0).children
     if (formbtns.item(0).classList.contains('form-submit-btn')) {
-      formbtns.item(0).focus() // Good UX
+      formbtns.item(0).focus() // Good UX?
       formbtns.item(0).click() // Send
     } // ignore?
   }
+}
+
+function hideSuggestions () {
+  const suggestions = document.getElementsByClassName('key-suggestion-wrap')
+  for (let i = 3; i < suggestions.length; i++) {
+    suggestions.item(i).add('key-suggestion-collapsed')
+    suggestions.item(i).add('hide')
+  }
+}
+
+function unhideSuggestions () {
+  const suggestions = document.getElementsByClassName('key-suggestion-wrap')
+  for (let i = 3; i < suggestions.length; i++) {
+    suggestions.item(i).add('key-suggestion-collapsed')
+    suggestions.item(i).remove('hide')
+  }
+}
+
+function hideSuggestion (itemid) {
+  // To collapse: key-suggestion-collapsed
+  const suggestion = document.getElementsByClassName('key-suggestion-wrap').item(itemid)
+  suggestion.classList.add('key-suggestion-collapsed')
+  suggestion.classList.add('hide')
+
+// // Add replacement node
+// <div class="key-suggestion-wrap key-suggestion-collapsed">
+//	<div class="key-suggestion clearfix">
+//		unhide
+//	</div>
+// </div>
+}
+
+function unhideSuggestion (itemid) {
+  // To collapse: key-suggestion-collapsed
+  const suggestion = document.getElementsByClassName('key-suggestion-wrap').item(itemid)
+  suggestion.classList.add('key-suggestion-collapsed')
+  suggestion.classList.remove('hide')
+
+// // Remove replacement node
+// <div class="key-suggestion-wrap key-suggestion-collapsed">
+//	<div class="key-suggestion clearfix">
+//		unhide
+//	</div>
+// </div>
 }
 
 /**
@@ -185,6 +229,26 @@ function deleteSuggestion (selected) {
     document.getElementsByClassName('ibtn key-suggestion-delete').item(selected).click()
   }
 } */
+
+/**
+ * Selects/Unselects all modified phrases on the import page.
+ */
+function importSelectAll () {
+  var phrases = document.getElementsByClassName('tr-plain-key-row') // phrases to be edited
+
+  if (phrases.length === document.getElementsByClassName('tr-plain-key-row selected').length) {
+    var allSelected = true
+  }
+  for (let i = 0; i < phrases.length; i++) {
+    if (allSelected) {
+      phrases.item(i).click() // unselect
+    } else {
+      if (!phrases.item(i).classList.contains('selected')) {
+        phrases.item(i).click()
+      }
+    }
+  }
+}
 
 /**
  * Applies the most popular translation, switches to next item
@@ -249,7 +313,7 @@ function handleShortcut (e) {
           if (!e.ctrlKey) {
             e.stopImmediatePropagation()
             e.preventDefault()
-            cancel() // needs @run-at  document-start to work
+            cancel() // needs "@run-at  document-start" to work
           } else return
           break
 
@@ -327,7 +391,14 @@ function handleShortcut (e) {
       if (!e.ctrlKey) addTranslation(); else matchedKey = false
       break
     case 'a':
-      if (e.ctrlKey) addTranslation(); else matchedKey = false
+      if (e.ctrlKey) {
+        // Select or Unselect all phrases on import page
+        if (window.location.href.includes('/import')) {
+          importSelectAll()
+        } else {
+          addTranslation()
+        }
+      } else matchedKey = false
       break
 
     // Edit translation
@@ -367,6 +438,7 @@ function handleShortcut (e) {
  */
 function init () {
   document.addEventListener('keydown', handleShortcut)
+  // use 'keyup' for single-key commands to prevent repeats?
 }
 
 init()
